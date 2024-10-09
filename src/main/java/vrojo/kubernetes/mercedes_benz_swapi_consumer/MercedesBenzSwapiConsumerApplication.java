54a3dc5fd908157b9626e5783eda72530a62a8bd
varojo <vrojo@uoc.edu>
2024-10-09 14:28:46 +0200

    First commit

---
+++ src/main/java/vrojo/kubernetes/mercedes_benz_swapi_consumer/MercedesBenzSwapiConsumerApplication.java
@@ -0,0 +1,13 @@
+package vrojo.kubernetes.mercedes_benz_swapi_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercedesBenzSwapiConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercedesBenzSwapiConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vrojo/kubernetes/mercedes_benz_swapi_consumer/MercedesBenzSwapiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package vrojo.kubernetes.mercedes_benz_swapi_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercedesBenzSwapiConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
